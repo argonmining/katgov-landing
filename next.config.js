3615a3a5abced17009f7956fb7907b007f04df46
argonmining <ashton@onargon.com>
2024-11-19 02:57:29 -0800

    build fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  // Add any other configurations you need
-}
+const nextConfig = {}
 
 module.exports = nextConfig
